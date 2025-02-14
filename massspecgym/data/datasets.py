3b088f7a19aa2628e2611018f2eabc6561f7a2fc
Jozefov <mn.mastaf@gmail.com>
2025-02-14 19:26:26 +0100

    limit cpus

--- massspecgym/data/datasets.py
+++ massspecgym/data/datasets.py
@@ -1253,13 +1253,7 @@ class MSnRetrievalDataset(MSnDataset):
 
         # Precompute candidate-side transformations in parallel.
         self.precomputed = {}
-        allocated_cpus = os.environ.get("SLURM_CPUS_ON_NODE")
-        if allocated_cpus is not None:
-            allocated_cpus = int(allocated_cpus)
-        else:
-            allocated_cpus = os.cpu_count()  # Fallback if not running under SLURM
-
-        # Use two less than allocated, but at least 1.
+        allocated_cpus = int(os.environ.get("SLURM_CPUS_ON_NODE", os.cpu_count()))
         num_workers = max(allocated_cpus - 2, 1)
         print(f"Using {num_workers} processes for precomputation.")
 
